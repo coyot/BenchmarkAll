c01b7ec7af09ec4b4d615ba64b892ce10620be1b
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2016-03-01 16:27:36 +0100

    Initial project

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            var r1 = !list.Any(t => t == 0);
+            var r2 = list.All(t => t != 0);
+
+            var summary = BenchmarkRunner.Run<TestClass>();
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/TestClass.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class TestClass
+    {
+        private const int N = 90000;
+        private IEnumerable<int> list;
+
+
+        public TestClass()
+        {
+            list = Enumerable.Range(0, N);
+        }
+
+        [Benchmark]
+        public bool All()
+        {
+            return list.All(i => i != 0);
+        }
+
+        [Benchmark]
+        public bool NotAny()
+        {
+            return !list.Any(i => i == 0);
+        }
+    }
+}
